f49e627f1e22e905df015b68e4d7f5842bb828af
Gary Ascuy <gary.ascuy@gmail.com>
2025-04-20 21:40:01 -0500

    Initial Commit

---
+++ NekoGui/NekoGuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  NekoGuiApp.swift
+//  NekoGui
+//
+//  Created by Gary Ascuy on 20/04/25.
+//
+
+import SwiftUI
+
+@main
+struct NekoGuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NekoGuiTests/NekoGuiTests.swift
@@ -0,0 +1,17 @@
+//
+//  NekoGuiTests.swift
+//  NekoGuiTests
+//
+//  Created by Gary Ascuy on 20/04/25.
+//
+
+import Testing
+@testable import NekoGui
+
+struct NekoGuiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
