5229b2a838ad04c9cb0a47aec2c50027d79e4bc3
Gary Ascuy <gary.ascuy@gmail.com>
2025-05-03 10:02:17 -0500

    Adding sidebar

--- NekoGui/ContentView.swift
+++ NekoGui/ContentView.swift
@@ -7,19 +7,44 @@
 
 import SwiftUI
 
+struct NekoItem: Identifiable, Hashable {
+    let id = UUID()
+    let name: String
+    
+    var items: [NekoItem]? = nil
+}
+
 struct ContentView: View {
+    @State private var collection = [
+        NekoItem(name: "Users"),
+        NekoItem(name: "Pets"),
+        NekoItem(name: "Create User"),
+        NekoItem(name: "Delete User")
+    ]
+    
+    @State private var selected: NekoItem?
+    @State var query: String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, neko!")
+        NavigationSplitView {
+            List(collection, selection: $selected) { item in
+                Text(item.name)
+            }
+            .searchable(text: $query, placement: .toolbar, prompt: Text("Search or Action"))
+        } content: {
+            if let item = selected {
+                Text(item.name)
+            } else {
+                Text("REQUEST")
+            }
+        } detail: {
+            Text("RESPONSE")
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
         .frame(minWidth: 700, minHeight: 400)
+        .navigationTitle("Neko")
 }
